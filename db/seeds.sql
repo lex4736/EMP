fb38c710333050dca4eeb0a10ac350b3a11403cd
Alexis Arando <lex4736@gmail.com>
2021-04-05 22:38:24 -0700

    setting up database

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+  
+INSERT INTO department (dept_name) VALUES ('Back of House'), ('Front of House'), ('Management');
+INSERT INTO company_role (title, salary, dept_id) VALUES
+('Owner', 100000.00, 1), 
+('Chef', 10.00, 2),
+('Host', 50.00, 3),
+
+
+INSERT INTO employees (first_name, last_name, emp_role_id, manager_id) VALUES
+('Eugene', 'Krabs', 1, null),
+('Spongebob', 'Squarepants', 2, 1),
+('Squidward', 'Tentacles', 3, 1);
+
+
